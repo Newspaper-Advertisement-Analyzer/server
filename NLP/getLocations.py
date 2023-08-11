392a1ac27632c1c628159ce154ce7b45ffdc1472
TharinduMadhusanka <108868341+TharinduMadhusanka@users.noreply.github.com>
2023-08-12 00:20:21 +0530

    Update getLocations.py

--- NLP/getLocations.py
+++ NLP/getLocations.py
@@ -1,7 +1,4 @@
-import ahocorasick
 import pickle
-
-# Generator function to read cities from the file one by one
 
 
 def cities_generator(filename):
@@ -9,40 +6,26 @@ def cities_generator(filename):
         for line in file:
             yield line.strip()
 
-# Initialize Aho-Corasick automaton and save it if not already saved
 
-
-def initialize_automaton(filename, save_filename):
+def initialize_automaton(filename):
     if not hasattr(initialize_automaton, 'cities'):
         try:
-            with open(save_filename, 'rb') as f:
+            with open(filename, 'rb') as f:
                 automaton = pickle.load(f)
         except (FileNotFoundError, pickle.UnpicklingError):
-            automaton = ahocorasick.Automaton()
-            for city in cities_generator(filename):
-                automaton.add_word(city, city)
-            automaton.make_automaton()
-            with open(save_filename, 'wb') as f:
-                pickle.dump(automaton, f)
-        initialize_automaton.automaton = automaton
-    return initialize_automaton.automaton
+            raise Exception("Automaton file not found or invalid.")
+        initialize_automaton.cities = automaton
+    return initialize_automaton.cities
 
 
 def extract_locations(given_text):
-    # Given text
-    # given_text = "This is a text containing some city names like Ampara, Kalmunai, etc., Colombo, Galle,  Ambagasdowa"
 
-    # Get the automaton (initialize if not already initialized)
-    # ===========================change the name later
-    automaton = initialize_automaton('cities.txt', 'cities.pkl')
+    automaton = initialize_automaton('cities.pkl')
 
-    # Find matching cities using the Aho-Corasick automaton
     matching_cities = set()
 
     for _, city in automaton.iter(given_text):
         matching_cities.add(city)
-
-    # print(matching_cities)
 
     if len(matching_cities) == 0:
         return ["No locations found"]
@@ -50,5 +33,6 @@ def extract_locations(given_text):
     return list(matching_cities)
 
 
-# print(extract_locations(
-#     "This is a text containing some city names like Ampara, Kalmunai, etc., Colombo, Galle,  Ambagasdowa Welimada"))
+
+
+
