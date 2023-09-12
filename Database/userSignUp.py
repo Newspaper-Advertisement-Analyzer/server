5764341665c7e88cd8387c71d9b5530f4db4528d
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2023-09-12 17:56:15 +0530

    sign IN process Updated

--- Database/userSignUp.py
+++ Database/userSignUp.py
@@ -4,39 +4,51 @@ from bson.json_util import dumps
 from dotenv import load_dotenv
 import os
 from flask_bcrypt import Bcrypt
+from Database.db_connector import db
+from datetime import datetime
 
 bcrypt = Bcrypt()
-load_dotenv('./.env')
+# load_dotenv('./.env')
 
-username: str = os.getenv('DBUSERNAME')
-password: str = os.getenv('PASSWORD')
+# username: str = os.getenv('DBUSERNAME')
+# password: str = os.getenv('PASSWORD')
 
-client = MongoClient("mongodb+srv://"+username+":"+password +"@cluster0.lemvb4s.mongodb.net/")
-db = client.NewspaperAdAnalyzer
+# client = MongoClient("mongodb+srv://"+username+":"+password +"@cluster0.lemvb4s.mongodb.net/")
+# db = client.NewspaperAdAnalyzer
 
-def add_user(name,email,password):
+def add_user(name, email, password):
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     try:
-        db.user.insert_one({"name":name,"email":email,"password":hashed_password})
+        db.User.insert_one({
+            "Full_Name": name,
+            "email": email,
+            "password": hashed_password,
+            "Contact_Number": None,
+            "User_Name": name,
+            "Registration_Date": datetime.now(),
+            "Profession": None
+        })
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 def delete_user(email):
-    db.user.delete_one({"email": email})
-    return True # not needed yet
+    try:
+        db.User.delete_one({"email": email})
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 def find_user(email):
-    user = db.user.find_one({"email":email})
+    user = db.User.find_one({"email": email})
     return user
 
-def validate_user(email,password):
-    user = db.user.find_one({"email":email})
+def validate_user(email, password):
+    user = db.User.find_one({"email": email})
     if user is None:
         return False
     if not bcrypt.check_password_hash(user["password"], password):
         return False
     return True
-
-
-    
--- blueprints/authentication/signin.py
+++ blueprints/authentication/signin.py
@@ -1,5 +1,33 @@
+# from flask import Blueprint, jsonify, request
+# from Database.userSignUp import add_user,find_user,validate_user
+
+# signIn_bp = Blueprint("signin", __name__)
+
+# @signIn_bp.route("/login", methods=["POST"])
+# def login_user():
+#     email = request.json["email"]
+#     password = request.json["password"]
+
+#     print("------------------login code is called------------------")
+#     print("email: ", email, "\n", "Pasword: ", password, "\n")
+
+#     if not email or not password:
+#         return jsonify({"error": "Email and password are required"}), 400
+
+#     user = find_user(email)
+#     if user is None:
+#         print("Invalid User Name")
+#         return jsonify({"error": "Invalid User Name"}), 401
+
+#     if validate_user(email, password):
+#         print("Correct Password")
+#         return jsonify({"message": "Success! You are now logged in."})
+#     else:
+#         print("Incorrect Password")
+#         return jsonify({"error": "Incorrect Password"}), 401
+
 from flask import Blueprint, jsonify, request
-from Database.userSignUp import add_user,find_user,validate_user
+from Database.userSignUp import add_user, find_user, validate_user
 
 signIn_bp = Blueprint("signin", __name__)
 
@@ -21,8 +49,16 @@ def login_user():
 
     if validate_user(email, password):
         print("Correct Password")
-        return jsonify({"message": "Success! You are now logged in."})
+        # Create a dictionary with user data (excluding password and _id)
+        user_data = {
+            "Full_Name": user["Full_Name"],
+            "email": user["email"],
+            "Contact_Number": user["Contact_Number"],
+            "User_Name": user["User_Name"],
+            "Registration_Date": user["Registration_Date"],
+            "Profession": user["Profession"]
+        }
+        return jsonify({"message": "Success! You are now logged in.", "user": user_data})
     else:
         print("Incorrect Password")
         return jsonify({"error": "Incorrect Password"}), 401
-
