355ccfcb78afeab003fa658e926b01649c101d49
TharinduMadhusanka <108868341+TharinduMadhusanka@users.noreply.github.com>
2023-09-12 17:27:10 +0530

    ocr pdf

--- NLP/pdf_to_text.py
+++ NLP/pdf_to_text.py
@@ -1,6 +1,7 @@
 import pytesseract
 from PIL import Image
 import PyPDF2
+import fitz  # PyMuPDF
 
 def extract_text_from_image(image_path):
     # Perform text extraction using Tesseract
@@ -27,4 +28,28 @@ def pdftotext(path):
     # closing the pdf file object
     data.close()
 
+    return text
+
+
+
+
+def pdftotext_ocr(pdf_path):
+    text = ""
+
+    pdf_document = fitz.open(pdf_path)
+
+    for page_number in range(len(pdf_document)):
+        page = pdf_document[page_number]
+        pix = page.get_pixmap()
+
+        # Convert the pixmap to a PIL (Pillow) image
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+
+        # Perform OCR on the image
+        page_text = pytesseract.image_to_string(img)
+
+        text += page_text
+        text += "\n"
+
+    pdf_document.close()
     return text
