fef8527ab1fb62a547a336a5811de93609e8bce6
TharinduMadhusanka <108868341+TharinduMadhusanka@users.noreply.github.com>
2023-09-13 13:32:27 +0530

    minor update

--- NLP/detailextract.py
+++ NLP/detailextract.py
@@ -8,6 +8,7 @@ from NLP.oldwebScaper import extract_paragraphs_and_list_items
 from NLP.oldwebScaper import extract_article_text
 from NLP.findprice import identify_price
 from NLP.findlocations_new import extract_locations
+from NLP.findcatogory import categorize_advertisement
 
 # Uncomment the line below if you haven't downloaded the model already
 #spacy.cli.download("en_core_web_sm")
@@ -28,24 +29,7 @@ def extract_email_addresses(text):
 #     # Example: regex to extract prices in the format "Rs 123.45 mn"
 #     return re.findall(r'Rs\s*(\d+(\.\d{1,2})?)\s*mn', text)
 
-def categorize_advertisement(advertisement_text):
-    category = ["Couldn't found a category"]
 
-    # Check for common keywords to categorize the advertisement
-    keywords_to_category = {
-        "Land Sales": ["land for sale", "landsales", "land plot", "property for sale", "land for", "land"],
-        "House Sales": ["house", "apartment"],
-        "Marriage Proposals": ["marriage proposal", "bride", "groom", "marriage partner", "matrimonial"],
-        "vehicle_for_sale": ["vehicle for sale", "car for sale", "auto for sale", "motorcycle for sale", "toyota"]
-    }
-
-    lowercased_text = advertisement_text.lower()
-    for cat, keywords in keywords_to_category.items():
-        if any(keyword in lowercased_text for keyword in keywords):
-            category = cat
-            break
-
-    return category
 
 
 
@@ -65,6 +49,7 @@ def analyze_advertisement(advertisement_URL):
     #     if ent.label_ == 'GPE':  # GPE label indicates locations
     #         location = ent.text.strip()
 
+    
     if extract_phone_num(advertisement_text) is not None:
         contact_info["phone_numbers"] = extract_phone_num(advertisement_text)
     else:
--- NLP/findcatogory.py
+++ NLP/findcatogory.py
@@ -1,13 +1,57 @@
-# def identify_catogory(text):
 
-#     candidate_labels = ["land", "house", "vehicle", "rent", "wedding"]
-#     text = text.lower()
+def categorize_advertisement(advertisement_text):
+    category = ["Couldn't found a category"]
 
-#     for label in candidate_labels:
-#         if label in text:
-#             return label
+    # Check for common keywords to categorize the advertisement
+    keywords_to_category = {
+        "Land Sales": ["land for sale", "landsales", "land plot", "property for sale", "land for", "land"],
+        "House Sales": ["house", "apartment"],
+        "Marriage Proposals": ["marriage proposal", "bride", "groom", "marriage partner", "matrimonial"],
+        "vehicle_for_sale": ["vehicle for sale", "car for sale", "auto for sale", "motorcycle for sale", "toyota"]
+    }
 
-#     return "Unclassified"
+    lowercased_text = advertisement_text.lower()
+    for cat, keywords in keywords_to_category.items():
+        if any(keyword in lowercased_text for keyword in keywords):
+            category = cat
+            break
+
+    return category
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def identify_catogory(text):
+
+    candidate_labels = ["land", "house", "vehicle", "rent", "wedding"]
+    text = text.lower()
+
+    for label in candidate_labels:
+        if label in text:
+            return label
+
+    return "Unclassified"
+
+
+
+
+
 def identify_catogory(advertisement_text):
     category = "Couldn't found a category"  # Default category
 
