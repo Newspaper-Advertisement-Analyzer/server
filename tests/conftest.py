9769493877d4d54d0370ba4f1337ee9162129eda
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2023-09-29 17:53:43 +0530

    unit test for sigin

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from pymongo import MongoClient
+from main import create_app
+
+
+@pytest.fixture(scope="module")
+def test_db():
+    # Connect to a test database (you may need to use a different database name)
+    client = MongoClient("mongodb://localhost:27017/")
+    test_db = client.TestNewspaperAdAnalyzer
+
+    yield test_db
+
+    # Clean up after tests
+    test_db.User.delete_many({})
+
+
+@pytest.fixture
+def client():
+    # Use the test database for the Flask app
+    app = create_app(config={"TESTING": True})
+    client = app.test_client()
+
+    yield client
