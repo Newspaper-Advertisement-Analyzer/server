6fa0bb83398210b3dfac3aa0d98fc6c3fb34004a
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2023-10-28 17:06:38 +0530

    Update db_connector

--- Database/db_connector.py
+++ Database/db_connector.py
@@ -8,7 +8,7 @@ load_dotenv('./.env')
 username = os.getenv('DBUSERNAME')
 password = os.getenv('PASSWORD')
 
-# client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.lemvb4s.mongodb.net/")
 client = MongoClient(
-    f"mongodb+srv://nadun:nadun2001@cluster0.lemvb4s.mongodb.net/")
+    f"mongodb+srv://{username}:{password}@cluster0.lemvb4s.mongodb.net/")
+
 db = client.Advizor
